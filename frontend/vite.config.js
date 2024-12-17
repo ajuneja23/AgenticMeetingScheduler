6298a11416d1575f77de7b286f83b9cf5ee18460
Aadit Juneja <aadit.juneja12@gmail.com>
2024-12-16 21:06:21 -0500

    adjust frontend favicon

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/AgenticMeetingScheduler",
+  root: ".",
+  build: {
+    outDir: "dist", // Ensure this is set to "dist"
+  },
+
   plugins: [react()],
   server: {
     port: 3000, // or any preferred port
